d763dc74d728659c35d44abe183b23300d6bce09
Ken-Winchester <tyz900720@gmail.com>
2021-10-19 21:31:21 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## HogwartsSDET18
+
+- [测试人社区](https://ceshiren.com)
+- [霍格沃兹测试学院](https://testerh.ke.qq.com)
+
+## 霍格沃兹测试学院学员ck181513演练作业
+
+## 作业提交地址
---
+++ sample_test.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @time :2021-10-19  21:24
+# @Author :Tyz
+# @Email :910771232@qq.com
+# @file :sample_test.py
+
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
