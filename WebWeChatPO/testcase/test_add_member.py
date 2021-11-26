bd1acecc72bf8043fe04d705af97ef581575e051
Ken-Winchester <tyz900720@gmail.com>
2021-11-26 20:40:45 +0800

    Optimized some files

--- WebWeChatPO/testcase/test_add_member.py
+++ WebWeChatPO/testcase/test_add_member.py
@@ -5,7 +5,6 @@
 # @file :test_add_member.py
 import pytest
 from selenium import webdriver
-
 from WebWeChatPO.page.contact_page import ContactPage
 from WebWeChatPO.page.main_page import MainPage
 
